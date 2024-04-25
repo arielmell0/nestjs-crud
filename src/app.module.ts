598294b179e89fe854ff765f44c8cc569b734d91
arielmell0 <arielmello@rede.ulbra.br>
2024-04-25 00:46:13 -0300

    feat: add movies create method

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,8 +4,26 @@ import { AppService } from './app.service';
 import { MoviesModule } from './movies/movies.module';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserEntity } from './users/entities/users.entity';
+import { MovieEntity } from './movies/entities/movie.entity';
 @Module({
-  imports: [UsersModule, MoviesModule, AuthModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      password: '1234',
+      username: 'postgres',
+      entities: [UserEntity, MovieEntity],
+      database: 'postgres',
+      synchronize: true,
+      logging: true,
+    }),
+    UsersModule,
+    MoviesModule,
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -5,20 +5,7 @@ import { UserEntity } from './entities/users.entity';
 import { UserController } from './users.controllers';
 
 @Module({
-  imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      password: '1234',
-      username: 'postgres',
-      entities: [UserEntity],
-      database: 'postgres',
-      synchronize: true,
-      logging: true,
-    }),
-    TypeOrmModule.forFeature([UserEntity]),
-  ],
+  imports: [TypeOrmModule.forFeature([UserEntity])],
   providers: [UsersService],
   controllers: [UserController],
   exports: [UsersService],
