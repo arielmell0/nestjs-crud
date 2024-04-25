38d483f02f8e9d1178bf0b21e31973f8644b11b9
arielmell0 <arielmello@rede.ulbra.br>
2024-04-25 02:15:19 -0300

    feat: swagger

--- src/movies/dto/create-movie.dto.ts
+++ src/movies/dto/create-movie.dto.ts
@@ -1,4 +1,7 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class CreateMovieDto {
+  @ApiProperty()
   readonly title: string;
   readonly description: string;
   readonly duration: number;
