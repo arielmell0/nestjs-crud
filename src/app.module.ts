2e79e79bc85ac5ee5ea05a630e9770cdb14979b0
arielmell0 <arielmello@rede.ulbra.br>
2024-05-16 00:44:06 -0300

    feat: dotenv implementation

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,16 +7,18 @@ import { UsersModule } from './users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserEntity } from './users/entities/users.entity';
 import { MovieEntity } from './movies/entities/movie.entity';
+import 'dotenv/config';
+
 @Module({
   imports: [
     TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      password: '1234',
-      username: 'postgres',
+      type: process.env.DB_TYPE,
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      password: process.env.DB_PASSWORD,
+      username: process.env.DB_USERNAME,
       entities: [UserEntity, MovieEntity],
-      database: 'postgres',
+      database: process.env.DB_DATABASE,
       synchronize: true,
       logging: true,
     }),
